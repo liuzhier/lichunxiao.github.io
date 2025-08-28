0f0297bb29fe7f5c53af3a3bc35dc85057385f40
lichunxiao <2298813582@qq.com>
2025-08-28 09:21:30 +0800

    add: the first submission of the project

---
+++ router/index.js
@@ -0,0 +1,7 @@
+// router/index.js
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHashHistory(), // 使用 Hash 模式
+  routes: [...]
+})
